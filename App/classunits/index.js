56836a6308d62ffd3397c897630407889fdb0793
Anders Lundström <anders@urkraft.se>
2016-12-28 13:41:58 +0100

    classunit med update och refaktorisering

--- App/classunits/index.js
+++ App/classunits/index.js
@@ -1,6 +1,12 @@
 (function () {
 "use strict"
 
+function findClassUnit(arr, id) {
+    return _.find(arr, function(a) {
+        return a.ClassUnitID == id;
+    });
+}
+
 angular.module("app")
 .component("classUnitIndex", {
     templateUrl: "/App/classunits/index.html",
@@ -8,25 +14,31 @@ angular.module("app")
     controllerAs: "model",
     controller: function (ClassUnit, Common) {
         var vm = this;
+
         this.add = function () {
-            var newclassUnit = new classUnit(vm.classUnit);
+            var newclassUnit = new ClassUnit(vm.ClassUnit);
             Common.AddEntity(newclassUnit, vm.classunits);
         }
         this.remove = function (classUnit) {
-            _.remove(vm.classunits, function(s){
-                return s.Id == classUnit.Id;
+            classUnit.$remove(function() {
+                _.remove(vm.classunits, function(s){
+                    return s.ClassUnitID == classUnit.ClassUnitID;
+                });
             });
         }
         this.startedit = function (classUnit) {
-            vm.classUnit = angular.copy(classUnit);
+            vm.ClassUnit = angular.copy(classUnit);
             vm.editing = true;
         }
         this.update = function () {
-            vm.classUnit.$update();
-            vm.clear();
+            vm.ClassUnit.$update(function(classunit) {
+                var classunit = findClassUnit(vm.classunits, classunit.ClassUnitID);
+                classunit.ClassName = vm.ClassUnit.ClassName;
+                vm.clear();
+            });
         }
         this.clear = function () {
-            vm.classUnit = new classUnit();
+            vm.ClassUnit = new ClassUnit();
             vm.editing = false;
         }
     }
