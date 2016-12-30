8a8197de6d1415e90e98de3247f3caecebb77278
Anders Lundström <lundstrom123@gmail.com>
2016-12-30 12:16:54 +0100

    gå till hemsidan som standard

--- App/logout/index.js
+++ App/logout/index.js
@@ -6,8 +6,9 @@ angular.module("app")
     template: "Du har loggats UT <br/>",
     // bindings: { students: '<' },
     // controllerAs: "model",
-    controller: function (Identity) {
+    controller: function (Identity, $state) {
         Identity.Logout();
+        $state.go('home')
         // window.location.href = '/Login/SecurityInfo'
     }
 });
